40e28f777de7f668a07880924eda24bd4af1bdf7
Thomas Sauvajon <thomas@sauvajon.tech>
2019-03-23 10:10:13 +0000

    Start the Go track

---
+++ go/two-fer/two_fer.go
@@ -0,0 +1,7 @@
+// Two-fer or 2-fer is short for two for one. One for you and one for me.
+package twofer
+
+// ShareWith with someone
+func ShareWith(name string) string {
+	return "One for " + name + ", one for me."
+}
