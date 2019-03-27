cb0e74aa45dc4e91d599d1df1d7aafb5bf7fef27
Thomas Sauvajon <thomas@sauvajon.tech>
2019-03-27 18:22:26 +0000

    use default values instead of an if cdtn

--- bash/two-fer/two_fer.sh
+++ bash/two-fer/two_fer.sh
@@ -1,18 +1,4 @@
 #!/usr/bin/env bash
 
-set -o errexit
-set -o nounset
-
-main() {
-  if [ "$#" -eq 0 ]; then
-    name="you"
-  else
-    name=$1
-  fi
-
-  echo "One for $name, one for me."
-  
-  return 0
-}
-
-main "$@"
+# if $1 doesn't contain a value, use "you" instead
+echo "One for ${1:-you}, one for me."
